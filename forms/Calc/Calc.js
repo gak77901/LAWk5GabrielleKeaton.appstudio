78c6b46b5f1275a2c05e645206feeb177305c60d
gak77901 <gak77901@creighton.edu>
2021-09-22 16:34:39 -0500

    Calc

---
+++ forms/Calc/Calc.js
@@ -0,0 +1,13 @@
+function calculate(num1,num2) {
+  let answer = num1 + num2
+  }
+  
+  let Number1 = prompt(`Enter your first number`)
+  let Number2 = prompt(`Enter your second number`)
+  let sum = calculate(Number1,Number2)
+  
+  console.log(`The sum of ${Number1} and ${Number2} is ${Sum}`)
+  
+  
+  
+  
